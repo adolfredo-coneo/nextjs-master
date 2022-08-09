72bb575b6832273eab9212637109621b36e30069
KOBEsphinx242 <ado.coneo@gmail.com>
2022-08-08 22:37:48 -0500

    Event detail

--- nextjs-events-project/pages/events/[eventId].tsx
+++ nextjs-events-project/pages/events/[eventId].tsx
@@ -1,10 +1,38 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import EventContent from '../../components/event-detail/event-content';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventSummary from '../../components/event-detail/event-summary';
+
+import { Event, getEventById } from '../../dummy-data';
 
 const EventDetailPage: NextPage = () => {
+  const [event, setEvent] = useState<Event | undefined>(undefined);
+  const { query } = useRouter();
+  const { eventId } = query;
+
+  useEffect(() => {
+    if (eventId) {
+      setEvent(getEventById(eventId as string));
+    }
+  }, [eventId]);
+
   return (
-    <div>
-      <h1>Events Detail Page</h1>
-    </div>
+    <>
+      {event && (
+        <>
+          <EventSummary title={event.title} />
+          <EventLogistics
+            date={event.date}
+            address={event.location}
+            image={event.image}
+            imageAlt={event.image}
+          />
+          <EventContent>{event.description}</EventContent>
+        </>
+      )}
+    </>
   );
 };
 
