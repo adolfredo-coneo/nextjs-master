1298345e0c0a3a9e12a05fe4f5607d30459b4ae3
KOBEsphinx242 <ado.coneo@gmail.com>
2022-08-28 10:14:58 -0500

    server side rendering

--- nextjs-events-project/pages/api.ts
+++ nextjs-events-project/pages/api.ts
@@ -22,3 +22,19 @@ export const getEventById = async (eventId: string) => {
   const allEvents = await getAllEvents();
   return allEvents.find((event) => event.id === eventId);
 };
+
+export const getFilteredEvents = async (dateFilter: {
+  year: any;
+  month: any;
+}) => {
+  const { year, month } = dateFilter;
+  const allEvents = await getAllEvents();
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+};
--- nextjs-events-project/pages/events/[...slug].tsx
+++ nextjs-events-project/pages/events/[...slug].tsx
@@ -1,59 +1,31 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import ErrorAlert from '../../components/ui/error-alert';
+import { getFilteredEvents } from '../api';
 
-import { getFilteredEvents } from '../../dummy-data';
+interface Props {
+  events: Array<any>;
+  error: string | undefined;
+  date: string | undefined;
+}
 
-const FilteredEventPage: NextPage = () => {
+const FilteredEventPage: NextPage<Props> = ({ events, error, date }) => {
   const router = useRouter();
   const filteredData = router.query.slug;
 
   if (!filteredData) {
-    return (
-      <>
-        <ErrorAlert>
-          <div className="center">No events found</div>
-        </ErrorAlert>
-      </>
-    );
+    return <div className="center">Loading...</div>;
   }
 
-  const year = +filteredData[0];
-  const month = +filteredData[1];
-  const date = new Date(year, month - 1);
-
-  if (
-    isNaN(year) ||
-    isNaN(month) ||
-    year > 2030 ||
-    year < 2021 ||
-    month < 1 ||
-    month > 12
-  ) {
+  if (error) {
     return (
       <>
         <ErrorAlert>
-          <div className="center">Invalid filter</div>
+          <div className="center">{error}</div>
         </ErrorAlert>
-        <ResultsTitle date={date} />
-      </>
-    );
-  }
-
-  const events = getFilteredEvents({
-    year: year,
-    month: month,
-  });
-
-  if (!events.length) {
-    return (
-      <>
-        <ErrorAlert>
-          <div className="center">No events found</div>
-        </ErrorAlert>
-        <ResultsTitle date={date} />
+        {date && <ResultsTitle date={date} />}
       </>
     );
   }
@@ -64,6 +36,61 @@ const FilteredEventPage: NextPage = () => {
       <EventList items={events} />
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const slug = context.query.slug;
+
+  if (!slug) {
+    return {
+      props: {
+        events: [],
+        error: 'No events found',
+      },
+    };
+  }
+
+  const year = +slug[0];
+  const month = +slug[1];
+  const date = new Date(year, month - 1).toDateString();
+
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12
+  ) {
+    return {
+      props: {
+        events: [],
+        error: 'Invalid filter',
+      },
+    };
+  }
+
+  const events = await getFilteredEvents({
+    year: year,
+    month: month,
+  });
+
+  if (!events.length) {
+    return {
+      props: {
+        events: [],
+        error: 'No events found',
+        date,
+      },
+    };
+  }
+
+  return {
+    props: {
+      events,
+      date,
+    },
+  };
 };
 
 export default FilteredEventPage;
