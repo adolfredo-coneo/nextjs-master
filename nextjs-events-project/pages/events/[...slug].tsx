7ca78ff00899509828df58a346af1dd1c743fed5
KOBEsphinx242 <ado.coneo@gmail.com>
2022-08-28 10:48:40 -0500

    add client fetching

--- nextjs-events-project/pages/events/[...slug].tsx
+++ nextjs-events-project/pages/events/[...slug].tsx
@@ -1,5 +1,8 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import ErrorAlert from '../../components/ui/error-alert';
@@ -7,33 +10,70 @@ import { getFilteredEvents } from '../api';
 
 interface Props {
   events: Array<any>;
-  error: string | undefined;
+  errorMSG: string | undefined;
   date: string | undefined;
+  year: number;
+  month: number;
 }
 
-const FilteredEventPage: NextPage<Props> = ({ events, error, date }) => {
+const FilteredEventPage: NextPage = () => {
+  const [eventsData, setEventsData] = useState([]);
   const router = useRouter();
   const filteredData = router.query.slug;
+
+  const { data, error } = useSWR(
+    'https://nextjs-course-47bf8-default-rtdb.firebaseio.com/events.json',
+    (url) => fetch(url).then((res) => res.json())
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedEvents = Object.keys(data).map((key) => ({
+        ...data[key],
+        id: key,
+      }));
+      setEventsData(transformedEvents as []);
+    }
+  }, [data]);
 
   if (!filteredData) {
     return <div className="center">Loading...</div>;
   }
 
-  if (error) {
+  const year = +filteredData[0];
+  const month = +filteredData[1];
+  const date = new Date(year, month - 1).toDateString();
+
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12 ||
+    error
+  ) {
     return (
       <>
         <ErrorAlert>
-          <div className="center">{error}</div>
+          <div className="center">Invalid</div>
         </ErrorAlert>
         {date && <ResultsTitle date={date} />}
       </>
     );
   }
 
+  const filteredEvents = eventsData.filter((event: any) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
   return (
     <>
       <ResultsTitle date={date} />
-      <EventList items={events} />
+      <EventList items={filteredEvents} />
     </>
   );
 };
@@ -45,7 +85,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {
       props: {
         events: [],
-        error: 'No events found',
+        errorMSG: 'No events found',
       },
     };
   }
@@ -65,7 +105,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {
       props: {
         events: [],
-        error: 'Invalid filter',
+        errorMSG: 'Invalid filter',
       },
     };
   }
@@ -79,8 +119,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     return {
       props: {
         events: [],
-        error: 'No events found',
+        errorMSG: 'No events found',
         date,
+        year,
+        month,
       },
     };
   }
@@ -89,6 +131,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     props: {
       events,
       date,
+      year,
+      month,
     },
   };
 };
