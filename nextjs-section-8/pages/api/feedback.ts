6863d512830c9a0adeab278514a0055de822186c
KOBEsphinx242 <ado.coneo@gmail.com>
2022-08-31 22:55:29 -0500

    add api feedback

--- nextjs-section-8/pages/api/feedback.ts
+++ nextjs-section-8/pages/api/feedback.ts
@@ -1,11 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
+import path from 'path';
 
 type Data = {
-    message: string
-  }
+  message: string;
+  feedback?: {
+    id: string;
+    email: string;
+    feedback: string;
+  };
+};
 
 function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ message: 'This works!!!' });
+  if (req.method === 'POST') {
+    const { email, feedback } = req.body;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      feedback,
+    };
+    const filePatch = path.join(process.cwd(), 'data', 'feedback.json');
+    const feedbacks = JSON.parse(fs.readFileSync(filePatch, 'utf8'));
+    feedbacks.push(newFeedback);
+    fs.writeFileSync(filePatch, JSON.stringify(feedbacks));
+
+    res
+      .status(201)
+      .json({ message: 'Thanks for the feedback!', feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: 'This works!!!' });
+  }
 }
 
 export default handler;
--- nextjs-section-8/pages/index.tsx
+++ nextjs-section-8/pages/index.tsx
@@ -1,12 +1,54 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import { useRef } from 'react';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const feedbackInputRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current?.value;
+    const enteredFeedback = feedbackInputRef.current?.value;
+
+    const requestBody = {
+      email: enteredEmail,
+      feedback: enteredFeedback,
+    };
+
+    fetch('/api/feedback', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className={styles.container}>
       <h1>The Home Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Your email address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea id="feedback" rows={5} ref={feedbackInputRef} />
+        </div>
+        <div>
+          <button type="submit">Send Feedback</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
